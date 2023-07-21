5052a1029ed7b2d596819764182d6b4e242b2c26
Noga25 <57655759+Noga25@users.noreply.github.com>
2023-07-21 22:46:02 +0300

    Sql server and the tables files

---
+++ DataBaseMananger.cs
@@ -0,0 +1,180 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Server
+{
+    public class DataBaseMananger
+    {
+        string con_str = "Server=localhost; database=game; UID=root; password=noga0000";
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataReader reader;
+
+        private void Disconnect()
+        {
+            con.Close();
+        }
+        private void Connect()
+        {
+            con = new MySqlConnection(con_str);
+            try
+            {
+                con.Open();
+            }
+            catch
+            {
+                con.Close();
+            }
+        }
+
+        public string GetPlayerName(int IdPlayers)
+        {
+            try
+            {
+                Connect();
+                string query = "SELECT Name FROM players WHERE IdPlayers =" + IdPlayers;
+                cmd = new MySqlCommand(query, con);
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    string name = reader.GetString("Name");
+                    Disconnect();
+                    return name;
+                }
+                else
+                {
+                    Disconnect();
+                    return null;
+                }
+            }
+            catch
+            {
+                Disconnect();
+                return null;
+            }
+
+        }
+
+        public string GetQuestion(int? QuestionID)
+        {
+            try
+            {
+                Connect();
+                QuestionID = 1;
+                string query = "SELECT questions FROM questions_table WHERE Id =" + QuestionID;
+                cmd = new MySqlCommand(query, con);
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    string Question = reader.GetString("questions");
+                    Disconnect();
+                    return Question;
+                }
+                else
+                {
+                    Disconnect();
+                    return null;
+                }
+            }
+            catch
+            {
+                Disconnect();
+                return null;
+            }
+
+        }
+
+        public string[] GetAnswers(int? AnswerID)
+        {
+            try
+            {
+                Connect();
+                AnswerID = 1;
+                string query = "SELECT CorrectAnswer, Answer2, Answer3, Answer4 FROM questions_table WHERE Id = " + AnswerID;
+                cmd = new MySqlCommand(query, con);
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    string CorrectAnswer = reader.GetString("CorrectAnswer");
+                    string Answer2 = reader.GetString("Answer2");
+                    string Answer3 = reader.GetString("Answer3");
+                    string Answer4 = reader.GetString("Answer4");
+                    Disconnect();
+                    return new string[] { CorrectAnswer, Answer2, Answer3, Answer4 };
+                }
+                else
+                {
+                    Disconnect();
+                    return null;
+                }
+            }
+            catch
+            {
+                Disconnect();
+                return null;
+            }
+
+        }
+
+        internal string SetPlayer(string name1, string name2)
+        {
+            string result;
+            try
+            {
+                Connect();
+                string query1 = "UPDATE players SET name = '" + name1 + "' WHERE IdPlayers = 1";
+                string query2 = "UPDATE players SET name = '" + name2 + "' WHERE IdPlayers = 2";
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = query1;
+                cmd.Parameters.AddWithValue("@name1", name1);
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = query2;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@name2", name2);
+                cmd.ExecuteNonQuery();
+
+                result = "Success";
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                Disconnect();
+                return result;
+            }
+            return result;
+        }
+
+        //public void SetPlayerName(string Name)
+        //{
+        //    try
+        //    {
+        //        Connect();
+        //        int IdPlayer = 1;
+        //        string query = "update players set name = " + Name + "WHERE IdPlayers = " + IdPlayer;
+        //        cmd = new MySqlCommand(query, con);
+        //        reader = cmd.ExecuteReader();
+        //        if (reader.Read())
+        //        {
+        //            Disconnect();
+        //        }
+        //        else
+        //        {
+        //            Disconnect();
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        Disconnect();
+        //    }
+
+        //}
+    }
+}
---
+++ game.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: game
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `IdPlayers` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  `Health` int DEFAULT NULL,
+  `ClassIf` int DEFAULT NULL,
+  `WEAPONSId` int DEFAULT NULL,
+  PRIMARY KEY (`IdPlayers`),
+  UNIQUE KEY `IdPlayers_UNIQUE` (`IdPlayers`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='This table stores all players data';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `players`
+--
+
+LOCK TABLES `players` WRITE;
+/*!40000 ALTER TABLE `players` DISABLE KEYS */;
+INSERT INTO `players` VALUES (1,'ido',2,2,1),(2,'Ilan',5,6,2),(3,'Or',5,12,1);
+/*!40000 ALTER TABLE `players` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `weapons`
+--
+
+DROP TABLE IF EXISTS `weapons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `weapons` (
+  `idWeapons` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) NOT NULL,
+  `Level` int DEFAULT NULL,
+  `RarityId` int DEFAULT NULL,
+  `Durubility` int DEFAULT NULL,
+  `Damamge` int DEFAULT NULL,
+  `TypeID` int DEFAULT NULL,
+  PRIMARY KEY (`idWeapons`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `weapons`
+--
+
+LOCK TABLES `weapons` WRITE;
+/*!40000 ALTER TABLE `weapons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `weapons` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-28 20:24:48
